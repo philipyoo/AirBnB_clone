0a63bd79b833c50e082485d3aa55fae1844377e8
Jianqin Wang <jianqinwang94@gmail.com>
2017-02-07 21:42:49 -0800

    now able to save and reload json files

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -6,20 +6,26 @@ class FileStorage:
     __file_path = "file.json"
     __objects = {}
 
+    def __init__(self):
+        self.reload()
+
     def all(self):
-        return self.__objects
+        return FileStorage.__objects
 
     def new(self, obj):
         if obj is not None:
-            self.__objects[obj.id] = obj.__dict__
+            FileStorage.__objects[obj.id] = obj.__dict__
 
     def save(self):
-        with open(self.__file_path, mode="w", encoding="utf-8") as fd:
-            fd.write(json.dumps(self.__objects))
+        with open(FileStorage.__file_path, mode="w", encoding="utf-8") as fd:
+            fd.write(json.dumps(FileStorage.__objects))
 
     def reload(self):
-        with open(self.__file_path, mode="w+", encoding="utf-8") as fd:
-            try:
-                self.__objects = json.load(fd)
-            except Exception:
-                pass
+        try:
+            with open(FileStorage.__file_path, mode="r+", encoding="utf-8") as fd:
+                try:
+                    FileStorage.__objects = json.load(fd)
+                except Exception as e:
+                    print("Error storing json:", e)
+        except Exception:
+            pass
