0371659cc53533c9f0624dcb3ea00f6b1890cf4f
Jianqin Wang <jianqinwang94@gmail.com>
2017-02-09 09:50:12 -0800

    updated console for new file_storage

--- console.py
+++ console.py
@@ -5,6 +5,7 @@ from models import *
 
 class ConsoleShell(cmd.Cmd):
     prompt = '(hbnb)'
+    storage.reload()
 
     def do_quit(self, args):
         """Quit command to exit the program"""
@@ -17,12 +18,54 @@ class ConsoleShell(cmd.Cmd):
 
     def do_create(self, args):
         """Create a new Basemodel"""
-        if len(args) <= 1:
+        args = args.split()
+        if len(args) != 1:
             print("Usage: create BaseModel")
+        else:
+            new_obj = BaseModel()
+            print(new_obj.id)
+            new_obj.save()
 
     def do_show(self, args):
-        pass
+        """Usage: show BaseModel 1234-1234-1234"""
+        args = args.split()
+        if len(args) != 2:
+            print("Usage: show BaseModel 1234-1234-1234")
+            return
+        all_objs = storage.all()
+        for objs_id in all_objs.keys():
+            if objs_id == args[1]:
+                print(all_objs[objs_id])
 
+    def do_destroy(self, args):
+        """Usage: destroy BaseModel 1234-1234-1234"""
+        args = args.split()
+        if len(args) != 2:
+            print("Usage: destroy BaseModel 1234-1234-1234")
+            return
+        all_objs = storage.all()
+        for objs_id in all_objs.keys():
+            if objs_id == args[1]:
+                del all_objs[objs_id]
+                break
+        storage.save()
+
+    def do_all(self, args):
+        """Usage: all Basemodel or all"""
+        all_objs = storage.all()
+        for objs_id in all_objs.keys():
+            print(all_objs[objs_id])
+
+    def do_update(self, args):
+        """Usage: update <class name> <id> <attribute name> <attribute value>"""
+        args = args.split()
+        if len(args) != 4:
+            print("Usage: see help for update")
+        all_objs = storage.all()
+        for obj_id in all_objs.keys():
+            if obj_id == args[1]:
+                setattr(all_objs[obj_id], args[2], args[3])
+        storage.save()
 
 if __name__ == '__main__':
     ConsoleShell().cmdloop()
