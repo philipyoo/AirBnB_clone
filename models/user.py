f83c920bda7403859fd6b78a8b76091434c20fca
philipyoo <philipyoo10@gmail.com>
2017-02-10 00:04:36 +0000

    Edit init file for models

--- models/user.py
+++ models/user.py
@@ -3,8 +3,8 @@ from models import *
 
 
 class User(BaseModel):
-    def __init__(self):
-        super().__init__()
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
         self.email = ""
         self.password = ""
         self.first_name = ""
