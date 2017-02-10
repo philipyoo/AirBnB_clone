60e69967b76c6d0184e9dec724184731b085477d
Jianqin Wang <jianqinwang94@gmail.com>
2017-02-09 16:10:11 -0800

    fix and update subclass modules

--- models/state.py
+++ models/state.py
@@ -3,5 +3,5 @@ from models import *
 
 class State(BaseModel):
     def __init__(self, *args, **kwargs):
-        super(State, self).__init__()
+        super(State, self).__init__(*args, **kwargs)
         self.name = ""
