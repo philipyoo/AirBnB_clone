932481fa73f33500219e436672a1b0dfd332b0f2
philipyoo <philipyoo10@gmail.com>
2017-02-10 22:50:44 +0000

    Create test console

---
+++ tests/test_console/consoletest.py
@@ -0,0 +1,19 @@
+import unittest
+from unittest.mock import create_autospec
+from models import *
+
+
+class Test_Console(unittest.TestCase):
+    """
+    Test the console
+    """
+
+    def setUp(self):
+        pass
+
+    def test_something(self):
+        pass
+
+
+if __name__ == "__main__":
+    unittest.main()
