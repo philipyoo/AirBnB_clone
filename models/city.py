28f0ac78f861f6bf21f4b8e01db3b09f6d439628
Jianqin Wang <jianqinwang94@gmail.com>
2017-02-11 16:10:57 -0800

    update model attr. from public ins. to public cls

--- models/city.py
+++ models/city.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 from models import *
 
+
 class City(BaseModel):
+    state_id = ""
+    name = ""
+
     def __init__(self, *args, **kwargs):
-        self.state_id = ""
-        self.name = ""
         super().__init__(*args, **kwargs)
