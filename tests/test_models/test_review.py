48009f221506b90ff94b438fb3785c0a7fd3ea61
philipyoo <philipyoo10@gmail.com>
2017-02-12 05:12:14 +0000

    Add review model class tests

---
+++ tests/test_models/test_review.py
@@ -0,0 +1,24 @@
+import unittest
+from datetime import datetime
+from models import *
+
+class Test_ReviewModel(unittest.TestCase):
+    """
+    Test the review model class
+    """
+
+    def setUp(self):
+        self.model = Review()
+        self.model.save()
+
+    def test_var_initialization(self):
+        self.assertTrue(hasattr(self.model, "place_id"))
+        self.assertTrue(hasattr(self.model, "user_id"))
+        self.assertTrue(hasattr(self.model, "text"))
+        self.assertEqual(self.model.place_id, "")
+        self.assertEqual(self.model.user_id, "")
+        self.assertEqual(self.model.text, "")
+
+
+if __name__ == "__main__":
+    unittest.main()
