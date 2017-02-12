941a378611a49b1d075f7b0a9c350fb0237dfabc
philipyoo <philipyoo10@gmail.com>
2017-02-12 04:58:45 +0000

    Add test for model user

---
+++ tests/test_models/test_user.py
@@ -0,0 +1,26 @@
+import unittest
+from datetime import datetime
+from models import *
+
+class Test_BaseModel(unittest.TestCase):
+    """
+    Test the base model class
+    """
+
+    def setUp(self):
+        self.model = User()
+        self.model.save()
+
+    def test_var_initialization(self):
+        self.assertTrue(hasattr(self.model, "email"))
+        self.assertTrue(hasattr(self.model, "password"))
+        self.assertTrue(hasattr(self.model, "first_name"))
+        self.assertTrue(hasattr(self.model, "last_name"))
+        self.assertEqual(self.model.email, "")
+        self.assertEqual(self.model.password, "")
+        self.assertEqual(self.model.first_name, "")
+        self.assertEqual(self.model.last_name, "")
+
+
+if __name__ == "__main__":
+    unittest.main()
