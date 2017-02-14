8cc07414a15b60b6f44049dc84e8caa853ad35bb
Jianqin Wang <jianqinwang94@gmail.com>
2017-02-13 23:30:33 -0800

    include documentation for model

--- models/base_model.py
+++ models/base_model.py
@@ -5,7 +5,9 @@ import models
 
 
 class BaseModel:
+    """The base class for all storage objects in this project"""
     def __init__(self, *args, **kwargs):
+        """initialize class object"""
         if len(args) > 0:
             for k in args[0]:
                 setattr(self, k, args[0][k])
@@ -16,15 +18,18 @@ class BaseModel:
             print("kwargs: {}: {}".format(k, kwargs[k]))
 
     def save(self):
+        """method to update self"""
         self.updated_at = datetime.datetime.now()
         models.storage.new(self)
         models.storage.save()
 
     def __str__(self):
+        """edit string representation"""
         return "[{}] ({}) {}".format(type(self)
                                      .__name__, self.id, self.__dict__)
 
     def to_json(self):
+        """convert to json"""
         dupe = self.__dict__.copy()
         dupe["created_at"] = str(dupe["created_at"])
         if ("updated_at" in dupe):
