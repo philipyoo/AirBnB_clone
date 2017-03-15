3776b56bab9ff772579eb9279e7b0c2c498f1b38
philipyoo <philipyoo10@gmail.com>
2017-03-15 20:09:10 +0000

    Edit file dir path

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -4,7 +4,7 @@ sudo apt-get update
 sudo apt-get -y install nginx
 sudo mkdir -p /data/web_static/{releases,shared,current}
 sudo mkdir -p /data/web_static/releases/test
-echo -e "<html>\n\t<head>\n\t</head>\n\t<body>\n\t\tHolberton School\n\t</body>\n</html>" | sudo tee /data/web_static/current/index.html
+echo -e "<html>\n\t<head>\n\t</head>\n\t<body>\n\t\tHolberton School\n\t</body>\n</html>" | sudo tee /data/web_static/releases/test/index.html
 sudo ln -sf /data/web_static/releases/test /data/web_static/current
 sudo chown -R ubuntu:ubuntu /data
 sudo sed -i '37i\\n\n\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current;\n\t\tautoindex off;\n\t}\n' /etc/nginx/sites-enabled/default
