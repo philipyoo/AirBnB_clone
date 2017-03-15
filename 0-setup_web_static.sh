fc924304dfdb128cb133eebe7d4ddca597849196
philipyoo <philipyoo10@gmail.com>
2017-03-15 18:50:28 +0000

    Edit web static server setup script

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 # Script to set up web servers for deployment of 'web_static' project
 sudo apt-get -y install nginx
-mkdir -p /data/web_static/{releases,shared,current}
-mkdir -p /data/web_static/releases/test
-echo -e "<html>\n\t<head>\n\t</head>\n\t<body>\n\t\tHolberton School\n\t</body>\n</html>" > /data/web_static/current/index.html
-ln -sf /data/web_static/releases/test /data/web_static/current
-chown -R ubuntu:ubuntu /data
+sudo mkdir -p /data/web_static/{releases,shared,current}
+sudo mkdir -p /data/web_static/releases/test
+echo -e "<html>\n\t<head>\n\t</head>\n\t<body>\n\t\tHolberton School\n\t</body>\n</html>" | sudo tee /data/web_static/current/index.html
+sudo ln -sf /data/web_static/releases/test /data/web_static/current
+sudo chown -R ubuntu:ubuntu /data
 sudo sed -i '37i\\n\n\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current;\n\t\tautoindex off;\n\t}\n' /etc/nginx/sites-enabled/default
 sudo service nginx restart
