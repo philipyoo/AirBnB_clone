34918c334d28e32097a00d4bd9e0ed05e533ed96
philipyoo <philipyoo10@gmail.com>
2017-03-15 23:11:22 +0000

    Add initial commit for 0x02-1

---
+++ 1-pack_web_static.py
@@ -0,0 +1,7 @@
+from fabric.api import local
+import datetime
+
+def do_pack():
+    local('mkdir -p versions')
+    timestamp = datetime.datetime.now().strftime("%Y%m%d%H%M%S")
+    local('tar -cvzf "versions/web_static_%s.tgz" ./web_static' % timestamp)
