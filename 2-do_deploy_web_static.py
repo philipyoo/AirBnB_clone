70876c2cec2214cbfe1c7961f7f6e6354638af56
philipyoo <philipyoo10@gmail.com>
2017-03-16 02:13:30 +0000

    Edit solution for 0x02-2

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -9,18 +9,15 @@ def do_deploy(archive_path):
     try:
         file_name = archive_path.split("/")[-1]
         file_without = file_name.split(".")[0]
+        dirname = "/data/web_static/releases/%s" % file_without
 
-        put(archive_path, "/tmp/")
-        run("sudo mkdir -p /data/web_static/releases/%s" % file_without)
-        run("sudo tar -xzf /tmp/%s -C /data/web_static/releases/%s/"
-            % (file_name, file_without))
+        put(archive_path, "/tmp")
+        run("sudo mkdir -p %s" % dirname)
+        run("sudo tar -xzf /tmp/%s -C %s" % (file_name, dirname))
         run("sudo rm /tmp/%s" % file_name)
-        run("""sudo mv /data/web_static/releases/%s/web_static/*
-        /data/web_static/releases/%s/""" % (file_without, file_without))
-        run("sudo rm -rf /data/web_static/releases/%s/web_static"
-            % file_without)
-        run("sudo ln -sf /data/web_static/releases/%s/ /data/web_static_current"
-            % file_without)
+        run("sudo mv %s/web_static/* %s/" % (dirname, dirname))
+        run("sudo rm -rf %s/web_static" % dirname)
+        run("""sudo ln -sf %s/ /data/web_static_current""" % dirname)
         return True
     except Exception:
         return False
