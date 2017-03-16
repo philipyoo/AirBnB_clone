365476f9022f2caf6b4c5cbdd8d7cf74c946f49e
philipyoo <philipyoo10@gmail.com>
2017-03-16 01:18:54 +0000

    Add solution for 0x02-2

---
+++ 2-do_deploy_web_static.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+
+from fabric.api import put, run, env
+from os.path import isfile
+
+env.hosts = ['75.101.171.13', '54.174.68.191']
+
+
+def do_deploy(archive_path):
+    file_name = archive_path.split("/")[-1]
+    file_without = file_name.split(".")[0]
+    if not isfile(archive_path):
+        return False
+
+    put(archive_path, "/tmp/%s" % file_name)
+    run("mkdir -p /data/web_static/releases/%s" % file_without)
+    run("tar -xzf /tmp/%s -C /data/web_static/releases/%s/"
+        % (file_name, file_without))
+    run("rm /tmp/%s" % file_name)
+    run("""mv /data/web_static/releases/%s/web_static/*
+    /data/web_static/releases/%s""" % (file_without, file_without))
+    run("rm -rf /data/web_static/releases/%s/web_static" % file_without)
+    run("rm -rf /data/web_static/current")
+    run("ln -s /data/web_static/releases/%s/ /data/web_static_current"
+        % file_without)
+
+    return True
